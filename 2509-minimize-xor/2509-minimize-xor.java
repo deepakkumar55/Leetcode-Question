bf206dc85f986271e02431a95b95a3d0d331637e
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2025-01-15 23:45:04 +0530

    Time: 0 ms (100%), Space: 40.5 MB (63.21%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimizeXor(int num1, int num2) {
+        int bitCount = Integer.bitCount(num2);
+        bitCount -= Integer.bitCount(num1);
+        int cur = 1;
+
+        while (bitCount != 0) {
+            if (bitCount < 0 && (num1 & cur) != 0) {
+                num1 ^= cur;
+                bitCount++;
+            } else if (bitCount > 0 && (num1 & cur) == 0) {
+                num1 |= cur;
+                bitCount--;
+            }
+            cur <<= 1;
+        }
+
+        return num1;
+    }
+}
